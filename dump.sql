fe3d9f96b70bd4b87336665e1d4750b9cb6d3b2f
shashwat-garg <shshwt.grg@gmail.com>
2017-11-21 09:28:20 +0530

    Final Project

---
+++ dump.sql
@@ -0,0 +1,379 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: Test
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `app`
+--
+
+DROP TABLE IF EXISTS `app`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app` (
+  `FriendName` varchar(255) NOT NULL,
+  PRIMARY KEY (`FriendName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app`
+--
+
+LOCK TABLES `app` WRITE;
+/*!40000 ALTER TABLE `app` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `appTags`
+--
+
+DROP TABLE IF EXISTS `appTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appTags` (
+  `Language` varchar(255) DEFAULT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  `Rating` decimal(3,1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appTags`
+--
+
+LOCK TABLES `appTags` WRITE;
+/*!40000 ALTER TABLE `appTags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `User1` varchar(255) DEFAULT NULL,
+  `User2` varchar(255) DEFAULT NULL,
+  `FileName` varchar(255) DEFAULT NULL,
+  KEY `User1` (`User1`),
+  KEY `User2` (`User2`),
+  CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`User1`) REFERENCES `user` (`Name`),
+  CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`User2`) REFERENCES `user` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat`
+--
+
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+INSERT INTO `chat` VALUES ('Shashwat','App','Shashwat+App.txt'),('Pratyush','App','Pratyush+App.txt'),('Pratyush','Shashwat','Pratyush+Shashwat.txt'),('Lasers67','App','Lasers67+App.txt'),('Lasers67','Pratyush','Lasers67+Pratyush.txt'),('Lasers67','Shashwat','Lasers67+Shashwat.txt'),('Palak Gupta','App','Palak Gupta+App.txt'),('Palak Gupta','Lasers67','Palak Gupta+Lasers67.txt'),('Palak Gupta','Pratyush','Palak Gupta+Pratyush.txt'),('Palak Gupta','Shashwat','Palak Gupta+Shashwat.txt'),('Mehul Raj Kumawat','App','Mehul Raj Kumawat+App.txt'),('Mehul Raj Kumawat','Lasers67','Mehul Raj Kumawat+Lasers67.txt'),('Mehul Raj Kumawat','Palak Gupta','Mehul Raj Kumawat+Palak Gupta.txt'),('Mehul Raj Kumawat','Pratyush','Mehul Raj Kumawat+Pratyush.txt'),('Mehul Raj Kumawat','Shashwat','Mehul Raj Kumawat+Shashwat.txt');
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Lasers67`
+--
+
+DROP TABLE IF EXISTS `Lasers67`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Lasers67` (
+  `FriendName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Lasers67`
+--
+
+LOCK TABLES `Lasers67` WRITE;
+/*!40000 ALTER TABLE `Lasers67` DISABLE KEYS */;
+INSERT INTO `Lasers67` VALUES ('Pratyush');
+/*!40000 ALTER TABLE `Lasers67` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Lasers67Tags`
+--
+
+DROP TABLE IF EXISTS `Lasers67Tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Lasers67Tags` (
+  `Language` varchar(255) NOT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Lasers67Tags`
+--
+
+LOCK TABLES `Lasers67Tags` WRITE;
+/*!40000 ALTER TABLE `Lasers67Tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Lasers67Tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Mehul Raj Kumawat`
+--
+
+DROP TABLE IF EXISTS `Mehul Raj Kumawat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Mehul Raj Kumawat` (
+  `FriendName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Mehul Raj Kumawat`
+--
+
+LOCK TABLES `Mehul Raj Kumawat` WRITE;
+/*!40000 ALTER TABLE `Mehul Raj Kumawat` DISABLE KEYS */;
+INSERT INTO `Mehul Raj Kumawat` VALUES ('Pratyush');
+/*!40000 ALTER TABLE `Mehul Raj Kumawat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Mehul Raj KumawatTags`
+--
+
+DROP TABLE IF EXISTS `Mehul Raj KumawatTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Mehul Raj KumawatTags` (
+  `Language` varchar(255) NOT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Mehul Raj KumawatTags`
+--
+
+LOCK TABLES `Mehul Raj KumawatTags` WRITE;
+/*!40000 ALTER TABLE `Mehul Raj KumawatTags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Mehul Raj KumawatTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Palak Gupta`
+--
+
+DROP TABLE IF EXISTS `Palak Gupta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Palak Gupta` (
+  `FriendName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Palak Gupta`
+--
+
+LOCK TABLES `Palak Gupta` WRITE;
+/*!40000 ALTER TABLE `Palak Gupta` DISABLE KEYS */;
+INSERT INTO `Palak Gupta` VALUES ('Pratyush'),('Shashwat');
+/*!40000 ALTER TABLE `Palak Gupta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Palak GuptaTags`
+--
+
+DROP TABLE IF EXISTS `Palak GuptaTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Palak GuptaTags` (
+  `Language` varchar(255) NOT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Palak GuptaTags`
+--
+
+LOCK TABLES `Palak GuptaTags` WRITE;
+/*!40000 ALTER TABLE `Palak GuptaTags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Palak GuptaTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Pratyush`
+--
+
+DROP TABLE IF EXISTS `Pratyush`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pratyush` (
+  `FriendName` varchar(255) NOT NULL,
+  PRIMARY KEY (`FriendName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pratyush`
+--
+
+LOCK TABLES `Pratyush` WRITE;
+/*!40000 ALTER TABLE `Pratyush` DISABLE KEYS */;
+INSERT INTO `Pratyush` VALUES ('Lasers67'),('Mehul Raj Kumawat'),('Palak Gupta'),('Shashwat');
+/*!40000 ALTER TABLE `Pratyush` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PratyushTags`
+--
+
+DROP TABLE IF EXISTS `PratyushTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PratyushTags` (
+  `Language` varchar(255) NOT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  `Rating` decimal(3,1) DEFAULT NULL,
+  PRIMARY KEY (`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PratyushTags`
+--
+
+LOCK TABLES `PratyushTags` WRITE;
+/*!40000 ALTER TABLE `PratyushTags` DISABLE KEYS */;
+INSERT INTO `PratyushTags` VALUES ('C++',2,3.3),('Language\n     \n   ',0,0.0),('Language\n           \n         ',0,0.0),('Python2.7',1,5.0);
+/*!40000 ALTER TABLE `PratyushTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Shashwat`
+--
+
+DROP TABLE IF EXISTS `Shashwat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Shashwat` (
+  `FriendName` varchar(255) NOT NULL,
+  PRIMARY KEY (`FriendName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Shashwat`
+--
+
+LOCK TABLES `Shashwat` WRITE;
+/*!40000 ALTER TABLE `Shashwat` DISABLE KEYS */;
+INSERT INTO `Shashwat` VALUES ('Palak Gupta'),('Pratyush');
+/*!40000 ALTER TABLE `Shashwat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ShashwatTags`
+--
+
+DROP TABLE IF EXISTS `ShashwatTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ShashwatTags` (
+  `Language` varchar(255) NOT NULL,
+  `TimesHelped` int(11) DEFAULT NULL,
+  `Rating` decimal(3,1) DEFAULT NULL,
+  PRIMARY KEY (`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ShashwatTags`
+--
+
+LOCK TABLES `ShashwatTags` WRITE;
+/*!40000 ALTER TABLE `ShashwatTags` DISABLE KEYS */;
+INSERT INTO `ShashwatTags` VALUES ('C++',0,0.0),('Java',0,0.0),('Language\n     \n   ',0,0.0),('Language\n           \n         ',0,0.0),('Python2.7',1,5.0);
+/*!40000 ALTER TABLE `ShashwatTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `Name` varchar(255) NOT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `SessionID` varchar(255) DEFAULT NULL,
+  `Online` int(11) DEFAULT NULL,
+  `Gender` varchar(7) DEFAULT NULL,
+  PRIMARY KEY (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('App','2345','aaa','VbbnkZkBvo_B4bJMAAAB',1,'Male'),('Lasers67','Lasers67','abcd@xyz.com','psMG58QWtDemIErEAAAA',0,NULL),('Mehul Raj Kumawat','Mehul','Mehul@iitmandi.ac.in','fkjxLX_PI3KzWPe3AAAE',0,NULL),('Palak Gupta','Palak889','Palak@Gupta.com','ACmuMQKeSmo1XKO7AAAH',0,NULL),('Pratyush','Pratyush100','Pratyushgauravgo@gmail.com','NF3scdzMMg6gNngcAAAD',1,'Male'),('Shashwat','Sg@iitmandi','shshwt.grg@gmail.com','QRbo70JLLpTxgLRbAAAC',0,'Male');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER after_user_insert
+AFTER INSERT ON user
+FOR EACH ROW
+BEGIN
+INSERT INTO chat(User1,User2,FileName) select new.Name as User1,user.Name as User2,concat(new.Name,'+',user.Name,'.txt') as FileName from user where new.Name!=user.Name;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-20  9:29:34
--- public/code.js
+++ public/code.js
@@ -1,92 +1,431 @@
 var Code=document.getElementById('Workspace');
+var user=document.getElementById('name');
 // Code.contentEditable=true;
 var Tabs=document.getElementById('tabs');
+//var Tabs_M=document.getElementById('tabs_M');
 var mybutton= document.getElementById('my_name');
+var mybutton2= document.getElementById('my_name2');
+var logoff=document.getElementById("signout");
+var compileButton=document.getElementById("Compile");
+var compileButton2=document.getElementById("Compile2");
+var searchUsers=document.getElementById('searchUsers');
+var language=0;
+var testCases=document.getElementById('Input');
+var output=document.getElementById('codeOutput');
 Code.focus();
+var users_in_room=document.getElementById('users_in_room');
+var users_in_room_M=document.getElementById('users_in_room_M');
+var leave_room=document.getElementById('leave_room');
+// var table=document.getElementById('tableToModify');
 var codingTogetherFlag=false;
 var codingTogetherHost=false;
-var original_room=user.innerHTML;
+var original_room=user.innerText;
 var current=original_room;
+function CodeTogetherRate(data){
+	var modal = document.getElementById('CodingTogetherRate');
+	var users=document.getElementById('CodeTogetherLangButtonRate');
+	var span = document.getElementById("closeRate");
+	var rate = document.getElementById("CodeTogetherRate");
+    var conf= document.getElementById("CodeTogetherRateSubmit");
+    modal.style.display = "block";
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+	conf.onclick =function(){
+		if(users.innerText==='Language')
+			console.log('Nothing chosen!');
+		else{
+			modal.style.display="none";
+			var d={
+				User:data.leaving,
+				lang:users.innerText,
+				rate:ratingValue
+			};
+			socket.emit('RatingUpdate',d);
+		}
+	}
+	window.onclick = function(event) {
+    	if (event.target == modal) {
+    	    modal.style.display = "none";
+   	 }
+	}
+}
+function LangChange(lang){
+	var x=document.getElementById(lang.id).innerHTML;
+	language=lang.id;
+	document.getElementById('LangButton').innerHTML=x+'<span class="caret"></span>';
+}
+function CodeTogetherLangChange(lang){
+	var x=document.getElementById(lang.id).innerHTML;
+	language=lang.id;
+	document.getElementById('CodeTogetherLangButton').innerHTML=x+'<span class="caret"></span>';
+}
+function CodeTogetherLangChangeRate(lang){
+	var x=document.getElementById(lang.id).innerHTML;
+	language=lang.id;
+	document.getElementById('CodeTogetherLangButtonRate').innerHTML=x+'<span class="caret"></span>';
+}
+compileButton2.onclick=function(){
+	if(language===0)
+		autolanguagedetection();
+	else
+		submitCode();
+}
 socket.on('connect',function(){
-	console.log('working');
-	socket.emit('room',{Room:original_room,to:user.innerHTML,from:user.innerHTML});
+	socket.emit('room',{Room:original_room,to:user.innerText,from:user.innerText});
 });
 //receive character from server
 socket.on('server_character',function(content){
-		console.log('From Server:'+content.Room);
 		if(content.Room===current)
-			Code.innerHTML=content.buffer;
+			Code.value=content.buffer;
 });
 Code.addEventListener('keyup',function(){
 	if(codingTogetherFlag==true){
-		console.log(current);	
-		console.log('emitting client_character');
-		var workCode=Code.innerHTML;
+		var workCode=Code.value;
 		socket.emit('client_character',{buffer:workCode, Room:current});
 	}
 });
-function codeTogether(otherUser){
+compileButton.onclick=function(){
+	if(language===0)
+		autolanguagedetection();
+	else
+		submitCode();
+}
+function codeTogetherOverlay(otherUser){
+	saveCode();
+	var modal = document.getElementById('CodingTogetherLang');
+	var users=document.getElementById('CodeTogetherLangButton');
+	var span = document.getElementById("closeLang");
+	var conf = document.getElementById("ConfirmCodeTogether");
+    modal.style.display = "block";
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+	conf.onclick =function(){
+		if(users.innerText==='Language');
+		else{
+			modal.style.display="none";
+			var d={
+				other:otherUser.id,
+				lang:users.innerText
+			};
+			codeTogether(d);
+		}
+	}
+	window.onclick = function(event) {
+    	if (event.target == modal) {
+    	    modal.style.display = "none";
+   	 }
+	}
+};
+function codeTogether(data){
 	saveCode();
 	codingTogetherFlag=true;
 	codingTogetherHost=true;
 	clickedMyButton();
-	var dat={
-		filen:user.innerHTML
+	var d={
+		filen:user.innerText,
 	};
-	socket.emit('takefilename',dat);
+	socket.emit('takefilename',d);
 	var dat={
-		fileName:user.innerHTML,
-		otherUser:otherUser.id,
+		fileName:user.innerText,
+		otherUser:(data.other).substr(0,(data.other).length-1),
+		lang: data.lang
 	};
 	socket.emit('codeTogether',dat);
 };
 function saveCode(){
 	var dat={
 		fileName:current,
-		code:Code.innerText
+		code:Code.value
 	};
 	socket.emit('saveFile',dat);
 };
-
-setInterval(saveCode, 100);
-
+logoff.onclick=function(){
+	saveCode();
+	this.href="http://localhost:4000/"; 
+};
+setInterval(saveCode, 10000);
+function submitCode()
+{
+	var dat={
+		source:Code.value,
+		testcases:testCases.value,
+		lang:language,
+		UserName:user.innerText
+	};
+	socket.emit('testCode',dat);
+}
 function clickedMyButton()
 {
+	leave_room.style.display='none';
 	codingTogetherHost=true;
-	current=user.innerHTML;
+	current=user.innerText;
+	mybutton.style.backgroundColor='black';
+	mybutton.style.color='white';
 	socket.emit('takefilename',{filen:current});
+	socket.emit('users_inside_this_room',current);
 }
 mybutton.onclick=function(){
+	leave_room.style.display='none';
+	codingTogetherHost=true;
+	document.getElementById(current+"1").style.backgroundColor='white';
+	document.getElementById(current+"1").style.color='black';
+	current=user.innerText;
+	this.style.backgroundColor='black';
+	this.style.color='white';
+	socket.emit('takefilename',{filen:current});
+	socket.emit('users_inside_this_room',current);
+};
+mybutton2.onclick=function(){
 	clickedMyButton();
-}
+};
 socket.on('codeTogether',function(data){
 	saveCode();
-	var choice=confirm('Want to code together with '+data.fileName+'?');
+	var choice=confirm('Want to code together with '+data.fileName+' in '+data.lang+'?');
 	// switch tabs on re Alert
 	if(choice==true){
+		socket.emit('codeTogetherUpdate',data);
 		var A=document.getElementById(data.fileName+"1");
 		if(A==null){
-					Code.innerText=data.code;
-					var but=document.createElement('button');
-					but.innerText=data.fileName;
-					but.id=data.fileName+"1";
-					but.onclick=function()
+		leave_room.style.display='block';
+					Code.value=data.code;
+					var list1=document.createElement('li');
+					var ref1=document.createElement('a');
+					ref1.innerHTML=data.fileName;
+					ref1.id=data.fileName+"1";
+					ref1.href="#";
+					ref1.onclick=function()
 					{ 
 						var string = this.id;
 						string = string.substr(0,string.length-1);
 						codingTogetherHost=false;
 						codingTogetherFlag=true;
 						current = string;
+						socket.emit('users_inside_this_room',current);
 						socket.emit('takefilename',{filen:current});
+						leave_room.style.display='block';
+					};
+					list1.appendChild(ref1);
+					var but=document.createElement('button');
+					but.innerText=data.fileName;
+					but.className="btn btn-default btn-arrow-right";
+					but.id=data.fileName+"1";
+					but.style.backgroundColor='black';
+					but.style.color='white';
+					if(current!=user.innerText)
+					{
+						document.getElementById(current+"1").style.backgroundColor='white';
+						document.getElementById(current+"1").style.color='black';
+					}
+					else
+					{
+						mybutton.style.backgroundColor='white';
+						mybutton.style.color='black';
+					}
+					but.onclick=function()
+					{ 
+						var string = this.id;
+						string = string.substr(0,string.length-1);
+						codingTogetherHost=false;
+						codingTogetherFlag=true;
+						if(current!=user.innerText)
+						{
+							document.getElementById(current+"1").style.backgroundColor='white';
+							document.getElementById(current+"1").style.color='black';
+						}
+						else
+						{
+							mybutton.style.backgroundColor='white';
+							mybutton.style.color='black';
+						}
+						current = string;
+						this.style.backgroundColor='black';
+						this.style.color='white';
+						socket.emit('users_inside_this_room',current);
+						socket.emit('takefilename',{filen:current});
+						leave_room.style.display='block';
 					};
 					Tabs.appendChild(but);
 		}
 		current=data.fileName;
 		codingTogetherHost=false;
 		codingTogetherFlag=true;
-		socket.emit('room',{Room:data.fileName,to:data.fileName,from:user.innerHTML});
+		socket.emit('room',{Room:data.fileName,to:data.fileName,from:user.innerText});
 	}
 });
 socket.on("takefilename",function(data){
-	Code.innerText=data;
+	Code.value=data;
+});
+function autolanguagedetection()
+{
+	var input = Code.value;
+	Algorithmia.client("simlsSH3xPkksEwSuBmAmXYlMHh1")
+    .algo("PetiteProgrammer/ProgrammingLanguageIdentification/0.1.3")
+    .pipe(input)
+    .then(function(output){
+    	var ID='0';
+        if(output.result[0][0]=='javascript'){
+        	ID='20';
+        }
+        else if(output.result[0][0]=='java'){
+        	ID='3';
+        }
+        else if(output.result[0][0]=='php'){
+        	ID='7';
+        }	
+        else if(output.result[0][0]=='c'){
+        	ID='1';
+        }
+        else if(output.result[0][0]=='lua'){
+        	ID='18';
+        	        }
+        else if(output.result[0][0]=='html'){
+        }
+        else if(output.result[0][0]=='objective-c'){
+        	ID='32';
+        }
+        else if(output.result[0][0]=='sql'){
+        	ID='10';
+        }
+        else if(output.result[0][0]=='css'){
+
+        }
+        else if(output.result[0][0]=='c++'){
+        	ID='2';
+        }
+        else if(output.result[0][0]=='swift'){
+        	ID='51';
+        }
+        else if(output.result[0][0]=='bash'){
+        	ID='14';
+        }
+        else if(output.result[0][0]=='ruby'){
+        	ID='8';
+        }
+        else if(output.result[0][0]=='perl'){
+        	ID='6';
+        }
+        else if(output.result[0][0]=='c#'){
+        	ID='9';
+        }
+        else if(output.result[0][0]=='scala'){
+        	ID='15';
+        }
+        else if(output.result[0][0]=='python'){
+        	ID='5';
+        }
+         else if(output.result[0][0]=='r'){
+         	ID='24';
+        }
+         else if(output.result[0][0]=='haskell'){
+         	ID='12';
+        }
+         else if(output.result[0][0]=='vb'){
+         	ID='37';
+        }
+        if(ID!='0')
+        {
+        	var to_change=document.getElementById(ID);
+        	LangChange(to_change);
+        }
+        submitCode();
+    });
+}
+function marzi(name){
+	var list=document.createElement('li');
+	var ref=document.createElement('a');
+	ref.innerHTML=name;
+	ref.id=name+"6";
+	ref.onclick=function(){
+		if(current===user.innerText && current!=this.innerHTML)
+		{
+			socket.emit('kicking',{kicked_person:this.innerHTML,from:user.innerText});
+		}
+		else{
+			alert("Apple");
+		}
+	};
+	list.appendChild(ref);
+	users_in_room_M.appendChild(list);
+	var row = document.createElement('tr');
+	var col = document.createElement('td');
+	col.innerText=name;
+	if(current==user.innerText && name!=user.innerText)
+		col.title='Kick '+name;
+	col.id=name+"6";
+	users_in_room.appendChild(row); 
+	col.onclick=function(){
+		if(current===user.innerText && current!=this.innerText)
+		{
+			socket.emit('kicking',{kicked_person:this.innerText,from:user.innerText});
+		}
+		else{
+			// alert("Cannot ");
+		}
+	};
+	row.appendChild(col);
+}
+socket.on('users_inside_this_room',function(data){
+	users_in_room.innerHTML='';
+	users_in_room_M.innerHTML='';
+	data.forEach(function(item){
+		marzi(item);
+	});
+});
+socket.on('kick_tab',function(data){
+	var a=document.getElementById(data+"1");
+	var b=document.getElementById(data+"6");
+	if(b!=null)
+	{
+		users_in_room.removeChild(b);
+		users_in_room_M.removeChild(b);
+	}
+	if(a!=null){
+		Tabs.removeChild(a);
+		//Tabs_M.removeChild(a);
+	}
+});
+leave_room.onclick=function(){
+	leave_room.style.display='none';
+	var a=document.getElementById(current+"1");
+	Tabs.removeChild(a);
+	//Tabs_M.removeChild(a);
+	socket.emit('left',{room:current, leaving:user.innerText});
+	clickedMyButton();
+}
+socket.on("got_kicked",function(data){
+	var a=document.getElementById(data+"1");
+	Tabs.removeChild(a);
+	//Tabs_M.removeChild(a);
+	clickedMyButton();
+});
+socket.on('byebye',function(data){
+	if(current===data)
+	{
+		clickedMyButton();
+	}
+});
+socket.on('testCode',function(data){
+	output.value=data;
+});
+socket.on('pleaseWait',function(){
+	output.value='Please Wait...';
+});
+
+socket.on('CodeTogetherEnd',function(data){
+	CodeTogetherRate(data);
+});
+socket.on('compileErrorResolve',function(data){
+	var doc=document.getElementById('response');
+	doc.innerHTML="<a href='"+data.url+"' style='color:white;' target='_blank'>Google says this</a><br/>";
+	if(data.arr.length==0){
+		doc.innerHTML+="<br>Try seeking help from your Friends!";
+	}
+	else{	
+		doc.innerHTML+="Or you can contact:";
+		data.arr.forEach(function(item){
+			doc.innerHTML+='<br/>'+item.FriendName;
+		});
+	}
 });
